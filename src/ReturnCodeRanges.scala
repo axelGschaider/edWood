fd86f5aaff7a61e3f776fd71ee14c2878d206129
axelGschaider <axel.gschaider@gmail.com>
2012-09-02 15:32:30 +0200

    some more RangeTesters and a ReturnCodeTester

--- src/ReturnCodeRanges.scala
+++ src/ReturnCodeRanges.scala
@@ -17,8 +17,28 @@ case class Negator(x:ReturnCodeRange) extends ReturnCodeRange {
   def contains(i:Int) = ! x.contains(i)
 }
 
-case class RangeList(x:List[ReturnCodeRange]) extends ReturnCodeRange {
-  def contains(i:Int) = x.exists(_.contains(i))
+case object AlwaysSuccess extends ReturnCodeRange {
+  def contains(i:Int) = true
 }
 
+case object AlwayFail extends ReturnCodeRange {
+  def contains(i:Int) = false
+}
 
+abstract class ReturnCodeTester(successRange:Option[ReturnCodeRange], errorRange: Option[ReturnCodeRange]) {
+  def success(i:Int) =  {
+    if(this.isSuccess(i)) true
+    else if(this.isError(i)) false
+    else this.isDefaultSuccess(i)
+  }
+
+  def isSuccess(i:Int) = successRange.map(_.contains(i)).getOrElse(false)
+  def isError(i:Int) = errorRange.map(_.contains(i)).getOrElse(false)
+  def isDefaultSuccess(i:Int) = i <= 0
+
+}
+
+case object DefaultCodeTester extends ReturnCodeTester(None, None)
+case class SuccessTester(successRange:ReturnCodeRange) extends ReturnCodeTester(Some(successRange), None)
+case class ErrorTester(errorRange:ReturnCodeRange) extends ReturnCodeTester(None, Some(errorRange))
+case class FullTester(successRange:ReturnCodeRange, errorRange:ReturnCodeRange) extends ReturnCodeTester(Some(successRange), Some(errorRange))
