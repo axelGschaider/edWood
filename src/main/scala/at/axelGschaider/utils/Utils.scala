776ac845c70240c5ee4251ff64f1451e03d08b7a
Axel Gschaider <axel.gschaider@gmail.com>
2012-11-05 21:19:32 +0100

    some tests and created basic Utils

---
+++ src/main/scala/at/axelGschaider/utils/Utils.scala
@@ -0,0 +1,8 @@
+package at.axelGschaider.utils
+
+object Utils {
+  
+  def ??? = throw new Exception("not implemented")
+  
+
+}
--- src/test/scala/edWood/config/ConfigValidatorTest.scala
+++ src/test/scala/edWood/config/ConfigValidatorTest.scala
@@ -1,5 +1,9 @@
 package edWood.config
 
+import edWood.reader.ReaderAdapter;
+import edWood.data._
+
+import at.axelGschaider.utils.Utils._
 
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
@@ -7,6 +11,8 @@ import org.scalatest.FunSuite
 
 @RunWith(classOf[JUnitRunner])
 class ConfigValidatorTest extends FunSuite {
+  
+  
 
  test("test IDs empty") {
    ConfigValidator.testIds(Nil)
@@ -14,7 +20,38 @@ class ConfigValidatorTest extends FunSuite {
  }
 
  test("single ID") {
+   
+   ConfigValidator.testIds(List(jobConf("lala")))
+   
+ }
  
+ 
+ def jobConfs(ids:List[String]) = ids.map(jobConf(_))
+ 
+ def jobConf(id:String) {
+   JobConfig(
+       "id",
+       Command("lalala", Nil, Nil, None),
+       dummyReader,
+       Nil,
+       Nil
+     )
+ }
+ 
+ val dummyReader = new ReaderAdapter() {
+   
+   override def getDescription() = "dummy"
+   
+   override def init(args:Array[String]) = {}
+   
+   override def readLife(input:LifeInput) = ???
+   
+   override def readPostRunSources(input:Array[Input]) = ???
+   
+   override def getData() = ???
+   
+   override def getDummyData() = ???
+   
  }
 
 }
