ce5f8e7d31814fd6af641b6986f6587bde43471e
Axel Gschaider <axel.gschaider@gmail.com>
2012-11-19 12:38:26 +0100

    object vs class

--- src/main/scala/edWood/returnCodes/SuccessMappingReader.scala
+++ src/main/scala/edWood/returnCodes/SuccessMappingReader.scala
@@ -11,3 +11,16 @@ import at.axelGschaider.loggsNProperties.DefaultLogs
 
 
 import scala.xml._
+
+object SuccessMappingReader extends DefaultLogs {
+  def read(xml:Elem, refs:Map[String, ReturnCodeRange]):SuccessMapping =
+    DefaultSuccess
+
+
+  def apply(refs:Map[String, ReturnCodeRange]) = new SuccessMappingReader(refs)
+}
+
+class SuccessMappingReader(refs:Map[String, ReturnCodeRange]) {
+  def read(xml:Elem) = SuccessMappingReader.read(xml, refs)
+}
+
