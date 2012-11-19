6219279c24e54d97ae9e853a2e8100fafa33165d
Axel Gschaider <axel.gschaider@gmail.com>
2012-11-19 22:27:28 +0100

    looks rather clean now

--- src/main/scala/edWood/returnCodes/SuccessMappingReader.scala
+++ src/main/scala/edWood/returnCodes/SuccessMappingReader.scala
@@ -21,68 +21,31 @@ object SuccessMappingReader extends DefaultLogs {
 
 
   def read(xml:Node, refs:RangeReference = Map()):SuccessMapping = 
-    children(xml).foldLeft(emptyRanges)( reduceToTupple(refs) ) match {
+    children(xml).foldLeft(emptyRanges)( reducer(refs) ) match {
       case (Nil, Nil) => DefaultSuccess
       case (suc, Nil) => JustSuccesses(suc)
       case (Nil, err) => JustErrors(err)
       case (suc, err) => FullMapping(suc, err)
     }
   
+  private val reducer = (refs:RangeReference) => 
+    reduceToTupple( resolveToRange(refs) )
 
-  private def reduceToTupple(refs:RangeReference) = (tuple:RangeTuple,xml:Node) =>
+  private def reduceToTupple(resolve:(Node => ReturnCodeRange)) = (tuple:RangeTuple,xml:Node) =>
     xml.label match {
       case "naming"  => tuple
-      case "success" => ( resolveToRange(xml,refs) :: tuple._1
+      case "success" => ( resolve(xml) :: tuple._1
                         , tuple._2   )
       case "failure" => ( tuple._1
-                        , resolveToRange(xml, refs) :: tuple._2 )
+                        , resolve(xml) :: tuple._2 )
       case someName  => throw new ReadXmlException("unknown tag <" + someName + "> in mapping definition")
     }
 
-  def resolveToRange(xml:Node, ref:RangeReference):ReturnCodeRange = ???
+  def resolveToRange( refs:RangeReference ) = (xml:Node) => attribute(xml, "ref") match {
+    case Some(ref) => 
+      refs.get(ref).getOrElse(throw new ReadXmlException("could not resolve ranges reference '" + ref + "'"))
+    case None      => ReturnCodeRangeReader read xml
+  }
 
 }
-//  def read(xml:Node, refs:Map[String, ReturnCodeRange]):Map[String, SuccessMapping] = {
-//    val elems = for {
-//      node  <- children(xml)
-//      label <- attribute(node, "label")
-//      _ = debug("resolving mapping '" + label + "'")
-//      mapping = readSingleMapping(node, refs)
-//    } yield (label, mapping)
-//
-//    Map() ++ elems
-//  }
-//
-//
-//  def readSingleMapping(xml:Node, refs:Map[String, ReturnCodeRange]):SuccessMapping = {
-//    val success = handleXmlList( (xml \ "success").toList, refs )
-//    val error = handleXmlList( (xml \ "error").toList, refs )
-//
-//    (success, error) match {
-//      case (Nil, Nil) => DefaultSuccess
-//      case (Nil, err) => JustErrors(err)
-//      case (suc, Nil) => JustSuccesses(suc)
-//      case (suc, err) => FullMapping(suc, err)
-//    }
-//
-//  }
-//
-//  private def handleXmlList( list:List[Node], refs:Map[String, ReturnCodeRange]) =
-//    list.map(xmlToRange(_, refs))
-//
-//  private def xmlToRange(xml:Node, refs:Map[String, ReturnCodeRange]): ReturnCodeRange =
-//    (xml \ "@ref").toList.headOption.map(_.toString) match {
-//      case Some(r) => 
-//          refs.get(r).getOrElse(throw new ReadXmlException("could not find referenced range '" + r + "'"))
-//      case None   => ReturnCodeRangeReader read xml
-//    }
-//
-//    
-//
-//  def apply(refs:Map[String, ReturnCodeRange]) = new SuccessMappingReader(refs)
-//}
-//
-//class SuccessMappingReader(refs:Map[String, ReturnCodeRange]) {
-//  def read(xml:Node) = SuccessMappingReader.read(xml, refs)
-//}
-//
+
