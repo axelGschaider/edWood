4bcce457548c571b8cab9b9471c5de9339c9e440
Axel Gschaider <axel.gschaider@gmail.com>
2012-11-19 22:52:33 +0100

    cleanup and a bunch of tests

--- src/main/scala/edWood/returnCodes/SuccessMappingReader.scala
+++ src/main/scala/edWood/returnCodes/SuccessMappingReader.scala
@@ -17,7 +17,7 @@ object SuccessMappingReader extends DefaultLogs {
 
   type RangeReference = Map[String, ReturnCodeRange]
   type RangeTuple = (List[ReturnCodeRange], List[ReturnCodeRange])
-  val emptyRanges:RangeTuple = (Nil, Nil)
+  private val emptyRanges:RangeTuple = (Nil, Nil)
 
 
   def read(xml:Node, refs:RangeReference = Map()):SuccessMapping = 
@@ -28,24 +28,30 @@ object SuccessMappingReader extends DefaultLogs {
       case (suc, err) => FullMapping(suc, err)
     }
   
-  private val reducer = (refs:RangeReference) => 
-    reduceToTupple( resolveToRange(refs) )
-
-  private def reduceToTupple(resolve:(Node => ReturnCodeRange)) = (tuple:RangeTuple,xml:Node) =>
-    xml.label match {
-      case "naming"  => tuple
-      case "success" => ( resolve(xml) :: tuple._1
-                        , tuple._2   )
-      case "failure" => ( tuple._1
-                        , resolve(xml) :: tuple._2 )
-      case someName  => throw new ReadXmlException("unknown tag <" + someName + "> in mapping definition")
-    }
-
   def resolveToRange( refs:RangeReference ) = (xml:Node) => attribute(xml, "ref") match {
     case Some(ref) => 
       refs.get(ref).getOrElse(throw new ReadXmlException("could not resolve ranges reference '" + ref + "'"))
     case None      => ReturnCodeRangeReader read xml
   }
+
+  private val reducer = (refs:RangeReference) => 
+    reduceToTupple( resolveToRange(refs) )
+
+
+  private def reduceToTupple(resolve:(Node => ReturnCodeRange)) = (tuple:RangeTuple,xml:Node) =>
+    xml.label match {
+      case "naming"  => tuple
+      case "success" => ( resolve(xml) :: tuple._1
+                        , tuple._2
+                        )
+      case "failure" => ( tuple._1
+                        , resolve(xml) :: tuple._2 
+                        )
+      case someName  => throw new ReadXmlException("unknown tag <" + someName + "> in mapping definition")
+    }
+
+
+
 
 }
 
--- src/test/scala/edWood/returnCodes/SuccessMappingReaderTest.scala
+++ src/test/scala/edWood/returnCodes/SuccessMappingReaderTest.scala
@@ -12,5 +12,56 @@ import org.scalacheck.Prop._
 @RunWith(classOf[JUnitRunner])
 class SuccessMappingReaderTest extends FunSuite {
 
+  test("just empty") {
+    val x = <a></a>
+    val m= SuccessMappingReader read x
+    assert(m success 0)
+    assert(!m.success(1))
+    assert(!m.success(2))
+  }
+
+  test("simple range success (naming is ignored)") {
+    val x = 
+      <a>
+        <success>4-8</success>
+        <naming label="never there">9</naming>
+      </a>
+    val m = SuccessMappingReader read x
+    assert(m success 0)
+    assert(m success 4)
+    assert(m success 6)
+    assert(m success 8)
+  }
+
+  test("mixed") {
+    val x = 
+      <a>
+        <success>4-8</success>
+        <failure>3,7</failure>
+        <success>
+          <and>
+            <d>1</d>
+            <e>9</e>
+          </and>
+        </success>
+      </a>
+    val m = SuccessMappingReader read x
+    //we made it so far. so it's ok
+  }
+
+  test("single reference (and tag val is ignored)") {
+    val x = 
+      <a>
+        <success ref="base">5</success>
+      </a>
+    val map = Map("base" -> Range(2,4))
+
+    val m = SuccessMappingReader.read(x, map)
+    assert(m success 0)
+    assert(m success 2)
+    assert(m success 4)
+    assert(!m.success(5))
+  }
+
 }
 
