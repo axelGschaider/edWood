48263e543988a7bc92f790d04489d6fa25f78ab7
Axel Gschaider <axel.gschaider@gmail.com>
2012-11-19 12:25:16 +0100

    empty files for SuccessMappingReader

---
+++ src/main/scala/edWood/returnCodes/SuccessMappingReader.scala
@@ -0,0 +1,13 @@
+package edWood.returnCodes
+
+
+import edWood.exceptions.ReadXmlException
+import edWood.xmlHelpers._
+import edWood.xmlHelpers.Implicits._
+import edWood.xmlHelpers.Utils._
+
+import at.axelGschaider.utils.Utils._
+import at.axelGschaider.loggsNProperties.DefaultLogs
+
+
+import scala.xml._
---
+++ src/test/scala/edWood/returnCodes/SuccessMappingReaderTest.scala
@@ -0,0 +1,16 @@
+package edWood.returnCodes
+
+import org.scalatest.junit.JUnitSuite
+import org.scalatest.prop.Checkers
+import org.junit.runner.RunWith
+import org.junit.Test
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
+import org.scalacheck.Arbitrary._
+import org.scalacheck.Prop._
+
+@RunWith(classOf[JUnitRunner])
+class SuccessMappingReaderTest extends FunSuite {
+
+}
+
