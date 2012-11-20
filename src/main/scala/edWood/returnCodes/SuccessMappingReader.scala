32a839ded08ba159be845fba0147fe3e960084a2
Axel Gschaider <axel.gschaider@gmail.com>
2012-11-20 18:31:38 +0100

    add a little scalaz goodness :)

--- src/main/scala/edWood/returnCodes/SuccessMappingReader.scala
+++ src/main/scala/edWood/returnCodes/SuccessMappingReader.scala
@@ -6,11 +6,14 @@ import edWood.xmlHelpers._
 import edWood.xmlHelpers.Implicits._
 import edWood.xmlHelpers.Utils._
 
+import scalaz.Scalaz._
+import scalaz._
+
 import at.axelGschaider.utils.Utils._
 import at.axelGschaider.loggsNProperties.DefaultLogs
 
 
-import scala.xml._
+import scala.xml.{Node => XNode}
 
 
 object SuccessMappingReader extends DefaultLogs {
@@ -20,7 +23,7 @@ object SuccessMappingReader extends DefaultLogs {
   private val emptyRanges:RangeTuple = (Nil, Nil)
 
 
-  def read(xml:Node, refs:RangeReference = Map()):SuccessMapping = 
+  def read(xml:XNode, refs:RangeReference = Map()):SuccessMapping = 
     children(xml).foldLeft(emptyRanges)( reducer(refs) ) match {
       case (Nil, Nil) => DefaultSuccess
       case (suc, Nil) => JustSuccesses(suc)
@@ -28,17 +31,16 @@ object SuccessMappingReader extends DefaultLogs {
       case (suc, err) => FullMapping(suc, err)
     }
   
-  def resolveToRange( refs:RangeReference ) = (xml:Node) => attribute(xml, "ref") match {
+  def resolveToRange( refs:RangeReference ) = (xml:XNode) => attribute(xml, "ref") match {
     case Some(ref) => 
       refs.get(ref).getOrElse(throw new ReadXmlException("could not resolve ranges reference '" + ref + "'"))
     case None      => ReturnCodeRangeReader read xml
   }
 
-  private val reducer = (refs:RangeReference) => 
-    reduceToTupple( resolveToRange(refs) )
+  private val reducer = (refs:RangeReference) => refs |> resolveToRange |> reduceToTupple
 
 
-  private def reduceToTupple(resolve:(Node => ReturnCodeRange)) = (tuple:RangeTuple,xml:Node) =>
+  private def reduceToTupple(resolve:(XNode => ReturnCodeRange)) = (tuple:RangeTuple,xml:XNode) =>
     xml.label match {
       case "naming"  => tuple
       case "success" => ( resolve(xml) :: tuple._1
