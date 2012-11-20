a040b80d0eeaf775107609971b88897afe8148fe
Axel Gschaider <axel.gschaider@gmail.com>
2012-11-20 23:37:01 +0100

    implicits to clean base code

--- src/main/scala/edWood/returnCodes/SuccessMappingReader.scala
+++ src/main/scala/edWood/returnCodes/SuccessMappingReader.scala
@@ -19,6 +19,11 @@ import scala.xml.{Node => XNode}
 object SuccessMappingReader extends DefaultLogs {
 
   type RangeReference = Map[String, ReturnCodeRange]
+  private case class RangeRefPimp(ref:RangeReference) {
+    def to(s:String) = 
+      ref.get(s).getOrElse(throw new ReadXmlException("could not resolve ranges reference '" + s + "'"))
+  }
+  private implicit def toPimpedRange(ref:RangeReference) = RangeRefPimp(ref)
 
   type RangeTuple = (List[ReturnCodeRange], List[ReturnCodeRange])
   private val emptyRanges:RangeTuple = (Nil, Nil)
@@ -26,18 +31,17 @@ object SuccessMappingReader extends DefaultLogs {
   def failure = Lens[RangeTuple, List[ReturnCodeRange]](_._2, (tuple, fail) => (tuple._1, fail))
 
 
-  def read(xml:XNode, refs:RangeReference = Map()):SuccessMapping = 
-    children(xml).foldLeft(emptyRanges)( reducer(refs) ) match {
+  def read(xml:XNode, ref:RangeReference = Map()):SuccessMapping = 
+    children(xml).foldLeft(emptyRanges)( reducer(ref) ) match {
       case (Nil, Nil) => DefaultSuccess
       case (suc, Nil) => JustSuccesses(suc)
       case (Nil, err) => JustErrors(err)
       case (suc, err) => FullMapping(suc, err)
     }
   
-  def resolveToRange( refs:RangeReference ) = (xml:XNode) => attribute(xml, "ref") match {
-    case Some(ref) => 
-      refs.get(ref).getOrElse(throw new ReadXmlException("could not resolve ranges reference '" + ref + "'"))
-    case None      => ReturnCodeRangeReader read xml
+  def resolveToRange( ref:RangeReference ) = (xml:XNode) => attribute(xml, "ref") match {
+    case Some(x) => ref to x
+    case None    => ReturnCodeRangeReader read xml
   }
 
   private val reducer = (refs:RangeReference) => refs |> resolveToRange |> reduceToTupple
